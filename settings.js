1fa34776316274fcdfce8b5a7add3ffded89b033
Rakib <rakib@Rakibs-MacBook-Air.local>
2023-11-29 23:42:59 +0600

    first

---
+++ settings.js
@@ -0,0 +1,71 @@
+interface Person {
+  name: string;
+  age: number;
+  profession: string;
+  id: string;
+}
+
+const peopleData: Person[] = [
+  {
+    name: "Emily Johnson",
+    age: 28,
+    profession: "Software Engineer",
+    id: "EJ123456",
+  },
+  {
+    name: "Daniel Martinez",
+    age: 35,
+    profession: "Marketing Manager",
+    id: "DM789012",
+  },
+  {
+    name: "Sophia Liu",
+    age: 40,
+    profession: "Dentist",
+    id: "SL345678",
+  },
+  {
+    name: "Miguel Rodriguez",
+    age: 32,
+    profession: "Graphic Designer",
+    id: "MR901234",
+  },
+  {
+    name: "Olivia Brown",
+    age: 45,
+    profession: "Financial Analyst",
+    id: "OB567890",
+  },
+  {
+    name: "Ethan Kim",
+    age: 29,
+    profession: "Product Manager",
+    id: "EK123789",
+  },
+  {
+    name: "Ava Patel",
+    age: 38,
+    profession: "Physician",
+    id: "AP456789",
+  },
+  {
+    name: "Liam Thompson",
+    age: 31,
+    profession: "Teacher",
+    id: "LT234567",
+  },
+  {
+    name: "Isabella Garcia",
+    age: 27,
+    profession: "Journalist",
+    id: "IG890123",
+  },
+  {
+    name: "Noah Wilson",
+    age: 33,
+    profession: "Civil Engineer",
+    id: "NW345678",
+  },
+];
+
+console.log(peopleData);
